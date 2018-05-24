234187d7cf90bb55edd20f63ce0cae104d58422f
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-24 15:01:45 -0400

    added checking mechanism for user input with while loop

--- percentage_grade.rb
+++ percentage_grade.rb
@@ -1,52 +1,41 @@
 def percentage_letter(per)
-  x = 1
-  while x == 1
-    if per >= 97 && per <= 100
-      return "A+"
-      x = 0
-    elsif per >= 93
-      return "A"
-      x = 0
-    elsif per >= 90
-      return "A-"
-      x = 0
-    elsif per >= 87
-      return "B+"
-      x = 0
-    elsif per >= 83
-      return "B"
-      x = 0
-    elsif per >= 80
-      return "B-"
-      x = 0
-    elsif per >= 77
-      return "C+"
-      x = 0
-    elsif per >= 73
-      return "C"
-      x = 0
-    elsif per >= 70
-      return "C-"
-      x = 0
-    elsif per >= 67
-      return "D+"
-      x = 0
-    elsif per >= 63
-      return "D"
-      x = 0
-    elsif per >= 60
-      return "D-"
-      x = 0
-    elsif per < 60
-      return "F"
-      x = 0
-    else
-      return "Not a valid percentage"
-    end
+  if per >= 97
+    return "A+"
+  elsif per >= 93
+    return "A"
+  elsif per >= 90
+    return "A-"
+  elsif per >= 87
+    return "B+"
+  elsif per >= 83
+    return "B"
+  elsif per >= 80
+    return "B-"
+  elsif per >= 77
+    return "C+"
+  elsif per >= 73
+    return "C"
+  elsif per >= 70
+    return "C-"
+  elsif per >= 67
+    return "D+"
+  elsif per >= 63
+    return "D"
+  elsif per >= 60
+    return "D-"
+  else
+    return "F"
   end
-
 end
 
-puts "Please enter a percentage"
-user_percentage = gets.to_i
-puts "\n#{user_percentage}% is a #{percentage_letter(user_percentage)}"
+x = "ON"
+while x == "ON"
+  puts "\nPlease enter a percentage:"
+  user_percentage = gets.to_i
+  if user_percentage > 100 || user_percentage < 0
+    puts "Not a valid option"
+  else
+    x = "OFF"
+  end
+end
+puts "\n#{user_percentage}% => #{percentage_letter(user_percentage)}\n\n"
